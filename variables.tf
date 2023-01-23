611d9285cec45b8c433f58ddfea40b2d3c8ae9ba
hubgited <edwardwangjq@gmail.com>
2023-01-23 12:16:25 -0800

    initial build

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  description = "name of the resource group"
+  default = "demo-rg"
+}
+
+variable "location" {
+  description = "location where this resource has to be created"
+  default = "westus3"
+}
+
+variable "tags" {
+  description = "tags name"
+  default = "demo"
+}

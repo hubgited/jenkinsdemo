5431fe5989dec82273217e59671fb7a388d7b664
hubgited <edwardwangjq@gmail.com>
2023-01-25 16:12:43 -0800

    update

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name = "rg-lz-000"
-        storage_account_name = "edtestsa01"
-        container_name = "tfstate"
-        key = "terraform.tfstate"
-    }
-}

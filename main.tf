d15c6dc8aa030cbd398a7645aae13a9b26148100
hubgited <edwardwangjq@gmail.com>
2023-01-25 16:11:36 -0800

    test

--- main.tf
+++ main.tf
@@ -1,14 +1,4 @@
-terraform {
-  required_version = ">= 0.14"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0.0, <4.0.0"
-    }
-  }
-}
 
-  
 resource "azurerm_resource_group" "rg" {
   name     = var.name
   location = var.location
